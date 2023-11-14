35bb28722861acb3fb01d045c91deba5ec41e03b
seanj29 <seanotto29@gmail.com>
2023-11-14 11:14:56 +0000

    nav bar horizantal changes

--- src/components/ProjectsSidebar.tsx
+++ src/components/ProjectsSidebar.tsx
@@ -1,23 +1,22 @@
-import { Link } from "gatsby"
 import React from "react"
 
 const ProjectsSidebar = () =>{
 
     return (
-    <nav className="inline-block items-center justify-between flex-wrap p-6">
-            <ul className="inline-block list-none">
+    <nav className="flex flex-col p-6">
+            <ul className="flex flex-col">
                 <li className="mt-4 mb-4 lg:inline-block lg:mr-2 lg:my-0">
-                    <a href="#Web"  className="transition-none hover:text-white hover:underline">
+                    <a href="#web"  className="transition-none hover:text-white hover:underline">
                         Web Development
                     </a>
                 </li>
                 <li className="mb-4 lg:inline-block lg:mr-2">
-                    <a href="#Game" className="transition-none hover:text-white hover:underline">
+                    <a href="#game" className="transition-none hover:text-white hover:underline">
                         Game Development
                     </a>
                 </li>
                 <li className="mb-4 lg:inline-block lg:mr-2">
-                    <a href="#Misc" className="transition-none hover:text-white hover:underline">
+                    <a href="#misc" className="transition-none hover:text-white hover:underline">
                         Misc Programming
                     </a>
                 </li>
