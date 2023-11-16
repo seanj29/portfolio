a83579f2b8e04dec9d9b7dc965f3c3e718113f61
seanj29 <seanotto29@gmail.com>
2023-11-16 20:28:58 +0000

    added the navbar functionality

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,26 +1,31 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import Header from "./header"
+
+
+
 const Navbar = () =>{
+    
+    const [nav, showNav] = useState(false)
 
     return (
-    <nav className="flex items-center justify-between flex-wrap p-6 bg-slate-900 lg:sticky lg:top-0">
+    <nav className="flex rounded-lg items-center justify-between flex-wrap p-6 bg-slate-900 lg:rounded-none lg:sticky lg:top-0">
         <div>
             <Header/>
         </div>
         <div className="block lg:hidden">
         <span className="sr-only">Open main menu</span>
-            <button className="flex items-center px-3 py-2 border rounded text-slate-400 border-gray-600 hover:text-white hover:border-white">
+            <button onClick={() => showNav(!nav)} className="flex items-center px-3 py-2 border rounded text-slate-400 border-gray-600 hover:text-white hover:border-white">
                 <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
                 </svg>
             </button>
         </div>
-        <div className="mt-4 flex w-full justify-start block text-3xl lg:text-xl lg:w-auto lg:justify-end">
+        <div className={`mt-4 flex w-full justify-start lg:block text-3xl lg:text-xl transition-transform lg:translate-y-0 lg:w-auto lg:justify-end ${ nav ? " block": "hidden" }`}>
             <ul className="block divide-y lg:divide-y-0">
                 <li className="py-2 lg:py-0 lg:inline-block lg:border-l lg:px-2">
-                    <Link to="/about" activeClassName="text-teal-600" className="hover:text-white hover:underline">
-                        About
+                    <Link to="/" activeClassName="text-teal-600" className="hover:text-white hover:underline">
+                        Home
                     </Link>
                 </li>
                 <li className="py-2 lg:py-0 lg:inline-block lg:border-l lg:px-2">
