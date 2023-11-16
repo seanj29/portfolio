d810930a3ad266be711bf60335bf0120d5ef5e53
seanj29 <seanotto29@gmail.com>
2023-11-16 22:02:40 +0000

    forgot to remove

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -24,7 +24,7 @@ const Navbar = () =>{
                 </svg>
             </button>
         </div>
-        <div className={`mt-4 flex w-full justify-start lg:block text-3xl lg:text-xl transition-transform lg:translate-y-0 lg:w-auto lg:justify-end ${ nav ? " block": "hidden" }`}>
+        <div className={`mt-4 flex w-full justify-start lg:block text-3xl lg:text-xl lg:w-auto lg:justify-end ${ nav ? " block": "hidden" }`}>
             <ul className="block divide-y lg:divide-y-0">
                 <li className="py-2 lg:py-0 lg:inline-block lg:border-l lg:px-2">
                     <Link to="/" activeClassName="text-teal-600" className="hover:text-white hover:underline">
