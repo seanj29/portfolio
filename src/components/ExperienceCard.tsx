a21fb04ce5438040ca5cb048a8faafd821a77a85
seanj29 <seanotto29@gmail.com>
2023-11-18 19:59:36 +0000

    added Experience

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,18 +1,33 @@
 import React from "react"
 
-
-
-const ExperienceCard = () =>{
+type ExperienceProps = {
   
+  title:string
+  otherRoles?: string
+  date:string,
+  desc: string,
+
+}
+
+
+
+const ExperienceCard = (props: ExperienceProps) =>{
+    const { title, date, otherRoles, desc } = props
     
       return (
-        <li className="grid sm:grid-cols-8 mb-10 mr-4 lg:ml-2">
-         <div>
-         Hiiii
-         </div>
-         <div>
-         Hiiii
-         </div>
+        <li className="grid sm:grid-cols-8 mb-10 sm:gap-8 md:gap-4">
+         <header className="font-semibold text-2xl uppercase text-slate-500 sm:col-span-2">
+          {date}
+         </header>
+         <div className="sm:col-span-6">
+          <h3 className="text-3xl text-slate-200">
+            {title}
+          </h3>
+          {otherRoles && <p className="my-2 text-xl">{otherRoles} </p>}
+          <p className="mt-1 text-lg whitespace-pre-line">
+          {desc}
+          </p>
+         </div> 
         </li>
       )
     }
--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -3,16 +3,27 @@ import type { HeadFC } from "gatsby"
 import { SEO } from "../components/seo";
 import Navbar from "../components/nav";
 import ExperienceCard from "../components/ExperienceCard";
+import { Experiences } from "../content/data.json"
 
 
 
 const ExperiencePage = () => {
     return(    
         <div className="mx-auto px-6 py-12">
-        <Navbar/>
-        <div className="mt-10 mx-6 lg:mx-20">
-            <ExperienceCard/>
-        </div>
+            <Navbar/>
+            <div className="mt-20 ml-6 lg:ml-15 max-w-screen-2xl">
+                <ol id="ExperienceList">
+                    {Experiences
+                    .map(experience => 
+                    (
+
+                        <ExperienceCard title={experience.title} date={experience.date} otherRoles={experience.otherRoles} desc={experience.desc}/>
+
+                    )
+                    )
+                    }
+                </ol>
+            </div>
         </div>
     )
 }
