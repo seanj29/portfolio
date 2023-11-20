2dff5c93e62a85d262a0370bed48872467301dfe
seanj29 <seanotto29@gmail.com>
2023-11-20 20:24:57 +0000

    finished contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react"
 import type { HeadFC } from "gatsby"
-import Navbar from "../components/nav";
-import { SEO } from "../components/seo";
+import Navbar from "../components/nav"
+import { SEO } from "../components/seo"
 
 
 const ContactPage = () =>{
 
     //
 
-  const [formurl, setFormUrl] = useState("https://docs.google.com/forms/d/e/1FAIpQLSfCHRDEFYbuwJ_AO0Mr2wlgFzDs6C8nhw6ncDplNNdJIEwpQA/formResponse?usp=pp_url")
+  const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfCHRDEFYbuwJ_AO0Mr2wlgFzDs6C8nhw6ncDplNNdJIEwpQA/viewform?usp=pp_url"
   const [nameValue, setNameValue] = useState("")
   const [emailValue, setEmailValue] = useState("")
   const [numberValue, setNumberValue] = useState("")
@@ -16,8 +16,7 @@ const ContactPage = () =>{
   const [messageValue, setMessageValue] = useState("")
 
   function handleFormSubmit(){
-    setFormUrl(`${formurl}&entry.1588175006=${encodeURI(nameValue)}&entry.1500911423=${encodeURI(subjectValue)}&entry.1931597131=${encodeURI(emailValue)}&entry.43927614=${encodeURI(numberValue)}&entry.142169962=${encodeURI(messageValue)}`)
-    console.log(formurl)
+    window.open(`${formUrl}&entry.1588175006=${encodeURI(nameValue)}&entry.1500911423=${encodeURI(subjectValue)}&entry.1931597131=${encodeURI(emailValue)}&entry.43927614=${encodeURI(numberValue)}&entry.142169962=${encodeURI(messageValue)}`, "_blank")
   }
 
   return (
@@ -26,8 +25,7 @@ const ContactPage = () =>{
       <Navbar/>
       <div className="mt-10 mx-6 lg:mx-20 bg-white dark:bg-gray-900 rounded-lg">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <form action="#" className="space-y-8" autoComplete="on" onSubmit={handleFormSubmit}
-              >
+          <form className="space-y-8" autoComplete="on" onSubmit={handleFormSubmit}>
               <div>
                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Full Name<span className="text-red-600"> *</span></label>
                 <input type="text" id="name" value={nameValue} onChange={(e) => setNameValue(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="John Smith" required/>
@@ -50,7 +48,7 @@ const ContactPage = () =>{
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Message<span className="text-red-600"> *</span></label>
                 <textarea id="message" value={messageValue} onChange={e => setMessageValue(e.target.value)} rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 " placeholder="Leave a comment..." required></textarea>
               </div>
-              <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-teal-700 sm:w-fit hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 disabled:cursor-not-allowed disabled:opacity-25 disabled:hover:bg-teal-700 disabled:dark:hover:bg-teal-600">Submit</button>
+              <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-teal-700 sm:w-fit hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 disabled:cursor-not-allowed disabled:opacity-25 disabled:hover:bg-teal-700 disabled:dark:hover:bg-teal-600">Submit (Brings you to google forms with the options filled out)</button>
           </form>
         </div>
       </div>
