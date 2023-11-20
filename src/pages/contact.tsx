0879fc25317a69c00dfadb0e5ea204de31d9845a
seanj29 <seanotto29@gmail.com>
2023-11-20 00:57:45 +0000

    working now somehow

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,29 +1,29 @@
 import React from "react"
 import type { HeadFC } from "gatsby"
-import Bio from "../components/bio";
 import Navbar from "../components/nav";
 import { SEO } from "../components/seo";
-import { BioText } from "../content/data.json"
-import Footer from "../components/socials";
 
-const FormPage = () =>{
+
+const ContactPage = () =>{
 
   return (
     
     <div className="mx-auto px-6 py-12">
       <Navbar/>
       <div className="mt-10 mx-6 lg:mx-20">
-        <h1 className="text-gray-200 text-4xl font-ubuntu font-semibold">
-          Hi there!
-        </h1>
-        <Bio TextContent={BioText}/>
-        <Footer/>
+      <form>
+        <label>
+          Name: test
+          <input type="text" value=""/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     </div>
   )
 }
 
-export default FormPage
+export default ContactPage
 
 export const Head: HeadFC = () => (
       <>
