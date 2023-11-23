4ee19b9056ddcdbf2e5d2a948093c6486e8b8ad8
seanj29 <seanotto29@gmail.com>
2023-11-23 00:19:55 +0000

    resyled for mobile

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -35,7 +35,7 @@ const ContactPage = () =>{
                 <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
                 <input type="text" id="subject" value={subjectValue} onChange={e => setSubjectValue(e.target.value)} autoComplete="off" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="What is your inquiry about?"/>
               </div>
-              <div className="inline-flex space-x-24">
+              <div className="space-y-8 lg:inline-flex lg:space-x-24 lg:space-y-0">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email<span className="text-red-600"> *</span></label>
                   <input type="email" id="email" value={emailValue} onChange={e => setEmailValue(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="name@email.com" required/>
