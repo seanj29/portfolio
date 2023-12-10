88b5d5ffda664d3e68f9b2630a21bb7a66ef50c4
seanj29 <seanotto29@gmail.com>
2023-12-10 22:40:27 +0000

    Update: Add White Color theme tailwind classes, changed instances of bg-slate-800 to bg-black or gray-900 and same with text

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -10,7 +10,7 @@ const Bio = (props: BioProps) =>{
 
     return (
     <div>
-        <main className="font-ubuntu text-black dark: text-slate-50/70 flex whitespace-pre-line mt-5">
+        <main className="font-ubuntu text-black dark:text-slate-50/70 flex whitespace-pre-line mt-5">
             <p>
                 {TextContent}
             </p>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,13 +6,13 @@ import React from "react"
 const Header = () =>{
 
     return (
-    <div className="text-gray-200 font-ubuntu">
+    <div className="text-black dark:text-gray-200 font-ubuntu">
         <h1 className="text-4xl font-bold">
             <Link to="/">
                 Sean Otto
             </Link>
         </h1>
-        <h2 className="text-slate-50/70 mt-3 txt-lg font-medium">
+        <h2 className="text-gray-900 dark:text-slate-50/70 mt-3 txt-lg font-medium">
             Software Engineer
         </h2>
     </div>
