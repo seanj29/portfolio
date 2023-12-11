b7b77c9f92f8d00b6caa39276b2ee226a176b015
seanj29 <seanotto29@gmail.com>
2023-12-11 11:30:46 +0000

    Fix: change icon color from text black to blue for light mode

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -24,7 +24,7 @@ const ExperiencePage = () => {
                     )
                     }
                 </ol>
-                <a href="../../resume.pdf" className="lg:ml-96 font-semibold text-lg text-white hover:text-black dark:text-black  dark:hover:text-white hover:underline">
+                <a href="../../resume.pdf" className="lg:ml-96 font-semibold text-lg text-black hover:text-blue-600 dark:hover:text-white hover:underline">
                             View Full Résumé
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline-block -mb-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
